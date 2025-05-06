6f4d0a130c259729078c72c2825788e2ee849538
Pares <alfariz2004@gmail.com>
2025-05-06 12:16:23 +0700

    done project and create own module

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const http = require('http');
 const url = require('url');
 
+const replaceTemplate = require('./modules/replaceTemplate');
+
 //////////////////
 // FILES
 
@@ -32,18 +34,7 @@ const url = require('url');
 
 //////////////////
 // SERVER
-const replaceTemplate = (temp, product) => {
-    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-    output = output.replace(/{%IMAGE%}/g, product.image);
-    output = output.replace(/{%PRICE%}/g, product.price);
-    output = output.replace(/{%FROM%}/g, product.from);
-    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-    output = output.replace(/{%QUANTITY%}/g, product.quantity);
-    output = output.replace(/{%DESCRIPTIONS%}/g, product.description);
 
-    if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-    return output
-}
 
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
 const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
@@ -54,7 +45,7 @@ const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
 
-    const pathname = req.url;
+    const {query, pathname} = url.parse(req.url, true);
 
     //OVERVIEW PAGE
     if (pathname === '/' || pathname === '/overview') {
@@ -62,12 +53,14 @@ const server = http.createServer((req, res) => {
 
         const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
         const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
-
         res.end(output);
 
     //PRODUCT PAGE
     } else if (pathname === '/product') {
-        res.end('this is the PRODUCT');
+        res.writeHead(200, { 'Conten-type': 'text/html' });
+        const product = dataObj [query.id];
+        const output = replaceTemplate(tempProduct, product);
+        res.end(output);
 
     // API 
     } else if (pathname === '/api') {
